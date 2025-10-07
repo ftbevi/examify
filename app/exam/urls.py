b4bb50cd3b61222e8c2b8f729669db132c43e720
Thiago Beviláqua <fthiagobevilaqua@gmail.com>
2025-10-07 08:56:16 -0300

    feat: start examify app

---
+++ app/exam/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import ExamViewSet, ExamResult, ExamSubmit
+
+
+router = DefaultRouter()
+router.register(r'exams', ExamViewSet, basename='exam')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path(
+        "<int:exam_id>/student/<int:student_id>/results/",
+        ExamResult.as_view(),
+        name="exam-student-result",
+    ),
+    path(
+        "<int:exam_id>/student/<int:student_id>/submit/",
+        ExamSubmit.as_view(),
+        name="exam-student-submit",
+    )
+]
